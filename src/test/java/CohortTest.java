f97add4b00a901adcf039f04a02fed6782b090ab
Roger Mayer <rmayer1984@gmail.com>
2019-12-17 10:27:30 -0600

    refractoring

--- src/test/java/CohortTest.java
+++ src/test/java/CohortTest.java
@@ -1,22 +1,17 @@
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 
 public class CohortTest {
-    @Test
-    public void testAddStudent(){
-        Cohort cohortWithOne = new Cohort();
-        assertEquals(0,cohortWithOne.getStudents().size());
-        cohortWithOne.addStudent(new Student(1,"Roger"));
-        assertEquals(1,cohortWithOne.getStudents().size());
-//        assertEquals(1,cohortWithOne.getStudents().get(0).getId());
-    }
-
-    @Test
-    public void testIfAverageIsCorrect(){
-        Cohort cohortWithMany = new Cohort();
-
+    Cohort emptyCohort;
+    Cohort cohortWithOne;
+    Cohort cohortWithMany;
+    @Before
+    public void setup(){
+        cohortWithOne = new Cohort();
+        cohortWithMany = new Cohort();
         Student roger = new Student(1,"Roger");
         roger.addGrade(100);
         Student riley = new Student(2,"Riley");
@@ -25,6 +20,25 @@ public class CohortTest {
         riley.addGrade(90);
 
         cohortWithMany.addStudent(new Student(1,"Roger"));
+    }
+
+    @Test
+    public void testAddStudent(){
+        Cohort cohortWithOne = new Cohort();
+        assertEquals(0,emptyCohort.getStudents().size());
+
+        assertEquals(1,cohortWithOne.getStudents().size());
+//        assertEquals(1,cohortWithOne.getStudents().get(0).getId());
+    }
+
+    @Test
+    public void testIfAverageIsCorrect(){
+        assertEquals(95.0, cohortWithMany.getCohortAverage(),0);
+
+
+
+
+
 
     }
 }
